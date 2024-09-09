30779c6aaa3ecf14706cb5b6ff33e6d8de0733b5
Roman Tereshkov <77459770+Bufu93@users.noreply.github.com>
2024-09-09 14:49:54 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import * as flsFunctions from './modules/functions.js'
+import * as navbar from './modules/navbar.js'
+
+flsFunctions.isWebp()
+navbar.burgerMenu()
+
+
+
+// import Swiper, { Navigation, Pagination } from 'swiper'
+
+// const swiper = new Swiper()
